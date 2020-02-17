fcb91d452b5332b67a259cf3a9c3141405c51a3c
Jesús Fernando Alvarez Franco <jefer_590@hotmail.com>
2020-02-16 20:59:53 -0500

    add metadata for twitter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,25 @@
 import { Configuration } from '@nuxt/types'
 
+const title = 'Rule of Three ⚡'
+const dek = 'Simple and Fast Rule of three tool ⚡'
+
 const config: Configuration = {
   mode: 'universal',
   head: {
-    title: 'Rule of Three ⚡',
+    title,
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Simple and Fast Rule of three tool ⚡' }
+      { hid: 'description', name: 'description', content: dek },
+      { name: 'twitter:card', hid: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', hid: 'twitter:site', content: '@jefer590' },
+      { name: 'twitter:title', hid: 'twitter:title', content: title },
+      { name: 'twitter:text:title', hid: 'twitter:text:title', content: title },
+      { name: 'twitter:description', hid: 'twitter:description', content: dek },
+      { name: 'og:title', hid: 'og:title', content: title }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
